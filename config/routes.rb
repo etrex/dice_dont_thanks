9993e99bbf43322d06e129c99899054c98e5e44f
etrex kuo <et284vu065k3@gmail.com>
2021-05-03 02:00:04 +0800

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
+  get "follow", to: "home#index"
+  get "join", to: "home#index"
   get "index", to: "home#index"
   get "menu", to: "home#index"
   get "目錄", to: "home#index"
